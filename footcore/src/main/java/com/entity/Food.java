44e570d72994d6ddd045078b27555d8603b41499
royzhuo <731924600@qq.com>
2018-09-17 18:10:49 +0800

    整合hibernate jpa

---
+++ footcore/src/main/java/com/entity/Food.java
@@ -0,0 +1,87 @@
+package com.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+/**
+ * Created by roy.zhuo on 2018/9/17.
+ */
+
+@Entity
+@Table(name = "food")
+public class Food {
+
+    @Id
+    private String id;
+    @Column
+    private String name;
+    @Column
+    private Double price;
+    @Column
+    private String address;
+    @Column
+    private String photoUrl;
+    @Column
+    private Date createTime;
+    @Column
+    private Date updateTime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
---
+++ footcore/src/main/java/com/entity/FoodType.java
@@ -0,0 +1,9 @@
+package com.entity;
+
+/**
+ * Created by roy.zhuo on 2018/9/17.
+ */
+
+
+public class FoodType {
+}
